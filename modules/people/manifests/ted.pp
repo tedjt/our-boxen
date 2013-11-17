30d8f1734b436f2e16deba859dbd2ea695495b40
Ted Tomlinson <zumbino@gmail.com>
2013-11-16 23:33:36 -0800

    update my manifest

--- modules/people/manifests/ted.pp
+++ modules/people/manifests/ted.pp
@@ -1,3 +1,11 @@
 class people::ted {
+  include sublime_text_2
+  include dropbox
+  include iterm2
+  include chrome
+  include heroku
+  include macvim
+  include virtualbox
+  include sourcetree
   include projects::all
 }
